22dba54daa78b67e605d8581d5a56a837bd7d2c0
Lauren Blake Hutchinson <70386624+lhutchinson22@users.noreply.github.com>
2023-06-12 23:45:21 +0000

    Updated title and color

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,8 +6,8 @@
         <div id="right"></div>
         <div id="top"></div>
         <div id="bottom"></div>
-        <b-navbar-item id="fontnavbar" class="title is-2">
-          Milligram
+        <b-navbar-item id="fontnavbar" class="title is-2" style="red">
+          Lauren's Milligram Page
         </b-navbar-item>
       </template>
     </b-navbar>
